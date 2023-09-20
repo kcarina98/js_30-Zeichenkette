4c90756c0c807b719f3198a13925791553891af6
Carina Klömpges <carinawerner25@gmail.com>
2023-09-20 12:11:13 +0200

    Kommentare für mich

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,14 @@ function clickme() {
   let first = document.querySelector("#zeichenkette").value;
   let second = document.querySelector("#trennen").value;
   let index = first.indexOf(second);
-  let cutDavor = first.slice(index);
-  let cutDanach = first.slice(index + 1);
 
+  // Cut für die erste Zeile
   let cut1 = first.substring(0, index);
   let cut2 = first.substring(0, index + 1);
+
+  // Cut für die zweite Zeile
+  let cutDavor = first.slice(index);
+  let cutDanach = first.slice(index + 1);
 
   // -Variable für Output
   let output = document.querySelector(".output");

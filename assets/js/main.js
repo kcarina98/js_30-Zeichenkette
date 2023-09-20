86d12595a4aa1a02c71f2380004501a76c266839
Carina Klömpges <carinawerner25@gmail.com>
2023-09-20 12:08:34 +0200

    Zeichenkette

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+function clickme() {
+  let first = document.querySelector("#zeichenkette").value;
+  let second = document.querySelector("#trennen").value;
+  let index = first.indexOf(second);
+  let cutDavor = first.slice(index);
+  let cutDanach = first.slice(index + 1);
+
+  let cut1 = first.substring(0, index);
+  let cut2 = first.substring(0, index + 1);
+
+  // -Variable für Output
+  let output = document.querySelector(".output");
+
+  // -Variablen für Radio Buttons
+  let davor = document.querySelector("#davor").checked;
+  let danach = document.querySelector("#danach").checked;
+
+  if (davor === true) {
+    output.innerHTML = `${cut1} <br> ${cutDavor}`;
+  } else if (danach === true) {
+    output.innerHTML = `${cut2} <br> ${cutDanach}`;
+  } else {
+    output.innerHTML = `Bitte wähle aus, ob vor oder nach "${second}" getrennt werden soll.`;
+  }
+}
